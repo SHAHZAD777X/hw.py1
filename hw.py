ffe2f35fca4b256ef6b68216cff8d07da74015ed
SHAHZAD777X <19241-sok@smgeducation.org>
2025-12-27 16:05:51 +0400

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,11 @@
+def productfun(um):
+    list1 = (list)
+    mult = 1
+    for a in list1:
+        mult=mult * a
+    return mult
+
+n = (2,3,-5)
+print("Tuple:",n)
+
+print("Product are:",productfun(n))
